359f6cf32e879206e990ad334818b79a3277d418
Xanokei <Geolee3D@Gmail.com>
2020-04-16 20:03:23 -0700

    Unity Files

---
+++ Assets/Tutorial/Scripts/pController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float thrust;
+
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //FixedUpdate
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * thrust);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pickup"))
+        { other.gameObject.SetActive(false); }
+    }
+}
